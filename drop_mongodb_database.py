a5c8c8617f26e15646d6b318f8c24e30296968a0
Kevin Koech <koechkevin92@gmail.com>
2023-08-07 13:00:43 +0300

    Post Deploy

---
+++ drop_mongodb_database.py
@@ -0,0 +1,19 @@
+def drop_mongodb_database(url):
+    try:
+        # Parse the MongoDB URL
+        parsed_url = urlparse(url)
+        username = parsed_url.username
+        password = parsed_url.password
+        hostname = parsed_url.hostname
+        port = parsed_url.port
+        db_name = parsed_url.path.replace("/", "")
+        client = pymongo.MongoClient(host=hostname, port=port,
+                                     username=username, password=password)
+        
+        db = client[db_name]
+        client.drop_database(db_name)
+        
+        print(f"Database '{db_name}' deleted successfully.")
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
